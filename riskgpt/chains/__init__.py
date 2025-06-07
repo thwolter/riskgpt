33c3a88a61a6e91ddc84c06ee575219e2f121236
Thomas Wolter <thwolter@gmail.com>
2025-06-07 14:05:55 +0200

    Add assessment chain for risk impact

--- riskgpt/chains/__init__.py
+++ riskgpt/chains/__init__.py
@@ -4,7 +4,7 @@ from .get_categories import get_categories_chain  # noqa: F401
 from .get_risks import get_risks_chain  # noqa: F401
 from .check_definition import check_definition_chain  # noqa: F401
 from .get_drivers import get_drivers_chain  # noqa: F401
-from .get_impact import get_impact_chain  # noqa: F401
+from .get_assessment import get_assessment_chain  # noqa: F401
 from .get_mitigations import get_mitigations_chain  # noqa: F401
 
 __all__ = [
@@ -13,6 +13,6 @@ __all__ = [
     "get_risks_chain",
     "check_definition_chain",
     "get_drivers_chain",
-    "get_impact_chain",
+    "get_assessment_chain",
     "get_mitigations_chain",
 ]
---
+++ riskgpt/chains/get_assessment.py
@@ -0,0 +1,28 @@
+from langchain_core.output_parsers import PydanticOutputParser
+
+from riskgpt.utils.prompt_loader import load_prompt
+from riskgpt.config.settings import RiskGPTSettings
+from riskgpt.models.schemas import AssessmentRequest, AssessmentResponse
+from riskgpt.registry.chain_registry import register
+from .base import BaseChain
+
+
+@register("get_assessment")
+def get_assessment_chain(request: AssessmentRequest) -> AssessmentResponse:
+    settings = RiskGPTSettings()
+    prompt_data = load_prompt("get_assessment")
+
+    parser = PydanticOutputParser(pydantic_object=AssessmentResponse)
+    chain = BaseChain(
+        prompt_template=prompt_data["template"],
+        parser=parser,
+        settings=settings,
+        prompt_name="get_assessment",
+    )
+
+    inputs = request.model_dump()
+    inputs["domain_section"] = (
+        f"Domain knowledge: {request.domain_knowledge}" if request.domain_knowledge else ""
+    )
+
+    return chain.invoke(inputs)
