459752ce3a9daab1f0a902dc95369166af9dd22b
Thomas Wolter <thwolter@gmail.com>
2025-06-28 07:39:12 +0200

    feat: add search module; expose search functionality and related models for external use

---
+++ src/riskgpt/helpers/search.py
@@ -0,0 +1,8 @@
+from riskgpt.helpers.search.__init__ import (
+    SearchRequest,
+    SearchResponse,
+    SearchResult,
+    search,
+)
+
+__all__ = ["search", "SearchRequest", "SearchResponse", "SearchResult"]
